990dfcf57e69f5b60656411550567e93a9300667
Yafim Dziuko <Yafim_Dziuko@epam.com>
2025-09-15 21:49:23 +0200

    task2

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
 
   return (
     <AppBar position="relative">
-      <Toolbar>
+      <Toolbar color="red">
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           <Link
             component={RouterLink}
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Yafim's Store!
           </Link>
         </Typography>
 
